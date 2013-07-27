0a95f424961c7aaa6c0449fe629b67ea16f83250
MasayaShimomatsu <masaya.shimomatsu@gmail.com>
2013-07-27 14:40:36 +0000

    my 3rd commit

---
+++ hw.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <alsa/asoundlib.h>
+
+int main()
+{
+ 
+ std::cout << "HELLO ALSA." << std::endl;
+ 
+ register int err;
+ int cardNum = -1;
+ 
+ for (;;) 
+ {
+ 
+   snd_ctl_t * cardHandle;
+   
+   if (( err = snd_card_next(&cardNum))< 0) {
+    break;
+   }
+   
+   if ( cardNum < 0 ) break;
+   
+   {
+   char str[26];
+   sprintf( str, "hw:%i", cardNum);
+   if (( err = snd_ctl_open( &cardHandle, str, 0)) < 0 ) {
+    printf("Can not open card %i: %s\n", cardNum, snd_strerror(err));
+   }
+   }
+   
+   {
+   snd_ctl_card_info_t *cardInfo;
+   snd_ctl_card_info_alloca(&cardInfo);
+   
+   if (( err = snd_ctl_card_info( cardHandle, cardInfo)) < 0 ){
+   	 printf("Cant get info for card %i: %s\n",cardNum,snd_strerror(err));
+   }else{
+   	 printf("Card %i = %s \n", cardNum, snd_ctl_card_info_get_name(cardInfo));
+   }
+   }
+   snd_ctl_close(cardHandle);
+ }
+ 
+ snd_config_update_free_global();
+ return 0;
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+hellomake : hw.cpp
+	 gcc -o hellommake hw.cpp -lstdc++ -lasound
+	
+
+hp : hw.cpp
+	g++ hw.cpp
